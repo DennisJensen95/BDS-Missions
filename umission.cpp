04c7a69a184cebd73dfe51716e72f9e0329fb663
Dennis <ngh1adj95@hotmail.com>
2020-02-28 10:47:44 +0100

    Testing commits

--- umission.cpp
+++ umission.cpp
@@ -18,8 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
-
 #include <sys/time.h>
 #include <cstdlib>
 
@@ -27,16 +25,15 @@
 #include "utime.h"
 #include "ulibpose2pose.h"
 
-
-UMission::UMission(UBridge * regbot, UCamera * camera)
+UMission::UMission(UBridge *regbot, UCamera *camera)
 {
   cam = camera;
   bridge = regbot;
   threadActive = 100;
   // initialize line list to empty
   for (int i = 0; i < missionLineMax; i++)
-  { // add to line list 
-    lines[i] = lineBuffer[i];    
+  { // add to line list
+    lines[i] = lineBuffer[i];
     // terminate c-strings strings - good practice, but not needed
     lines[i][0] = '\0';
   }
@@ -44,30 +41,28 @@ UMission::UMission(UBridge * regbot, UCamera * camera)
   th1 = new thread(runObj, this);
 }
 
-
 UMission::~UMission()
 {
   printf("Mission class destructor\n");
 }
 
-
 void UMission::run()
 {
   while (not active and not th1stop)
     usleep(100000);
-//   printf("UMission::run:  active=%d, th1stop=%d\n", active, th1stop);
+  //   printf("UMission::run:  active=%d, th1stop=%d\n", active, th1stop);
   if (not th1stop)
     runMission();
   printf("UMission::run: mission thread ended\n");
 }
-  
+
 void UMission::printStatus()
 {
   printf("# ------- Mission ----------\n");
   printf("# active = %d, finished = %d\n", active, finished);
   printf("# mission part=%d, in state=%d\n", mission, missionState);
 }
-  
+
 /**
  * Initializes the communication with the robobot_bridge and the REGBOT.
  * It further initializes a (maximum) number of mission lines 
@@ -85,7 +80,7 @@ void UMission::missionInit()
   // the mission is started, but staying in place (velocity=0, so servo action)
   //
   bridge->send("robot <add thread=1\n");
-  // Irsensor should be activated a good time before use 
+  // Irsensor should be activated a good time before use
   // otherwise first samples will produce "false" positive (too short/negative).
   bridge->send("robot <add irsensor=1,vel=0:dist<0.2\n");
   //
@@ -119,11 +114,10 @@ void UMission::missionInit()
   bridge->event->clearEvents();
 }
 
-
-void UMission::sendAndActivateSnippet(char ** missionLines, int missionLineCnt)
+void UMission::sendAndActivateSnippet(char **missionLines, int missionLineCnt)
 {
-  // Calling sendAndActivateSnippet automatically toggles between thread 100 and 101. 
-  // Modifies the currently inactive thread and then makes it active. 
+  // Calling sendAndActivateSnippet automatically toggles between thread 100 and 101.
+  // Modifies the currently inactive thread and then makes it active.
   const int MSL = 100;
   char s[MSL];
   int threadToMod = 101;
@@ -147,10 +141,10 @@ void UMission::sendAndActivateSnippet(char ** missionLines, int missionLineCnt)
   // send mission lines using '<mod ...' command
   for (int i = 0; i < missionLineCnt; i++)
   { // send lines one at a time
-    if (strlen((char*)missionLines[i]) > 0)
+    if (strlen((char *)missionLines[i]) > 0)
     { // send a modify line command
-      snprintf(s, MSL, "<mod %d %d %s\n", threadToMod, i+1, missionLines[i]);
-      bridge->send(s); 
+      snprintf(s, MSL, "<mod %d %d %s\n", threadToMod, i + 1, missionLines[i]);
+      bridge->send(s);
     }
     else
       // an empty line will end code snippet too
@@ -158,13 +152,12 @@ void UMission::sendAndActivateSnippet(char ** missionLines, int missionLineCnt)
   }
   // let it sink in (10ms)
   usleep(10000);
-  // Activate new snippet thread and stop the other  
+  // Activate new snippet thread and stop the other
   snprintf(s, MSL, "<event=%d\n", startEvent);
   bridge->send(s);
   // save active thread number
   threadActive = threadToMod;
 }
-
 
 //////////////////////////////////////////////////////////
 
@@ -208,7 +201,7 @@ void UMission::runMission()
   }
   if (not bridge->info->isHeartbeatOK())
   { // heartbeat should come at least once a second
-    system("espeak \"Oops, no usable connection with robot.\" -ven+f4 -s130 -a60 2>/dev/null &"); 
+    system("espeak \"Oops, no usable connection with robot.\" -ven+f4 -s130 -a60 2>/dev/null &");
     bridge->send("oled 3 Oops: Lost REGBOT!");
     printf("# ---------- error ------------\n");
     printf("# No heartbeat from robot. Bridge or REGBOT is stuck\n");
@@ -225,7 +218,7 @@ void UMission::runMission()
     { // just wait, do not continue mission
       usleep(20000);
       if (not inManual)
-        system("espeak \"Mission paused.\" -ven+f4 -s130 -a40 2>/dev/null &"); 
+        system("espeak \"Mission paused.\" -ven+f4 -s130 -a40 2>/dev/null &");
       inManual = true;
       bridge->send("oled 3 GAMEPAD control\n");
     }
@@ -236,7 +229,7 @@ void UMission::runMission()
         // - in response to 'bot->send("start\n")' earlier
         if (bridge->event->isEventSet(33))
         { // start mission (button pressed)
-//           printf("Mission::runMission: starting mission (part from %d to %d)\n", fromMission, toMission);
+          //           printf("Mission::runMission: starting mission (part from %d to %d)\n", fromMission, toMission);
           regbotStarted = true;
         }
       }
@@ -248,24 +241,24 @@ void UMission::runMission()
           system("espeak \"Mission resuming.\" -ven+f4 -s130 -a40 2>/dev/null &");
           bridge->send("oled 3 running AUTO\n");
         }
-        switch(mission)
+        switch (mission)
         {
-          case 1: // running auto mission
-            ended = mission1(missionState);
-            break;
-          case 2:
-            ended = mission2(missionState);
-            break;
-          case 3:
-            ended = mission3(missionState);
-            break;
-          case 4:
-            ended = mission4(missionState);
-            break;
-          default:
-            // no more missions - end everything
-            finished = true;
-            break;
+        case 1: // running auto mission
+          ended = mission1(missionState);
+          break;
+        case 2:
+          ended = mission2(missionState);
+          break;
+        case 3:
+          ended = mission3(missionState);
+          break;
+        case 4:
+          ended = mission4(missionState);
+          break;
+        default:
+          // no more missions - end everything
+          finished = true;
+          break;
         }
         if (ended)
         { // start next mission part in state 0
@@ -282,14 +275,12 @@ void UMission::runMission()
           bridge->send(s);
           if (logMission != NULL)
           {
-            fprintf(logMission, "%ld.%03ld %d %d\n", 
+            fprintf(logMission, "%ld.%03ld %d %d\n",
                     t.getSec(), t.getMilisec(),
-                    missionOld, missionStateOld
-            );
-            fprintf(logMission, "%ld.%03ld %d %d\n", 
+                    missionOld, missionStateOld);
+            fprintf(logMission, "%ld.%03ld %d %d\n",
                     t.getSec(), t.getMilisec(),
-                    mission, missionState
-            );
+                    mission, missionState);
           }
           missionOld = mission;
           missionStateOld = missionState;
@@ -335,11 +326,10 @@ void UMission::runMission()
   }
   bridge->send("stop\n");
   snprintf(s, MSL, "espeak \"%s finished.\"  -ven+f4 -s130 -a12  2>/dev/null &", bridge->info->robotname);
-  system(s); 
+  system(s);
   printf("Mission:: all finished\n");
   bridge->send("oled 3 finished\n");
 }
-
 
 ////////////////////////////////////////////////////////////
 
@@ -349,246 +339,66 @@ void UMission::runMission()
  *              therefore defined as reference with the '&'.
  *              State will be 0 at first call.
  * \returns true, when finished. */
-bool UMission::mission1(int & state)
+bool UMission::mission1(int &state)
 {
   bool finished = false;
   // First commands to send to robobot in given mission
   // (robot sends event 1 after driving 1 meter)):
   switch (state)
   {
-    case 0:
-      // tell the operatior what to do
-      printf("# press green to start.\n");
-      system("espeak \"press green to start\" -ven+f4 -s130 -a5 2>/dev/null &"); 
-      bridge->send("oled 5 press green to start");
-      state++;
-      break;
-    case 1:
-      if (bridge->joy->button[BUTTON_GREEN])
-        state = 10;
-      break;
-    case 10: // first PART - wait for IR2 then go fwd and turn
-      snprintf(lines[0], MAX_LEN, "vel=0 : ir2 < 0.3");
-      // drive straight 0.6m - keep an acceleration limit of 1m/s2 (until changed)
-      snprintf(lines[1], MAX_LEN, "vel=0.2,acc=1:dist=0.6");
-      // stop and create an event when arrived at this line
-      snprintf(lines[2], MAX_LEN, "event=1, vel=0");
-      // add a line, so that the robot is occupied until next snippet has arrived
-      snprintf(lines[3], MAX_LEN, ": dist=1");
-      // send the 4 lines to the REGBOT
-      sendAndActivateSnippet(lines, 4);
-      // make sure event 1 is cleared
-      bridge->event->isEventSet(1);
-      // tell the operator
-      printf("# case=%d sent mission snippet 1\n", state);
-      system("espeak \"code snippet 1.\" -ven+f4 -s130 -a5 2>/dev/null &"); 
-      bridge->send("oled 5 code snippet 1");
-      //
-      // play as we go
-      play.setFile("../The_thing_goes_Bassim.mp3");
-      play.setVolume(5); // % (0..100)
-      play.start();
-      // go to wait for finished
-      state = 11;
-      featureCnt = 0;
-      break;
-    case 11:
-      // wait for event 1 (send when finished driving first part)
-      if (bridge->event->isEventSet(1))
-      { // finished first drive
-        state = 999;
-        play.stopPlaying();
-      }
-      break;
-    case 999:
-    default:
-      printf("mission 1 ended \n");
-      bridge->send("oled 5 \"mission 1 ended.\"");
-      finished = true;
-      break;
-  }
-  return finished;
-}
-
-
-/**
- * Run mission
- * \param state is kept by caller, but is changed here
- *              therefore defined as reference with the '&'.
- *              State will be 0 at first call.
- * \returns true, when finished. */
-bool UMission::mission2(int & state)
-{
-  bool finished = false;
-  // First commands to send to robobot in given mission
-  // (robot sends event 1 after driving 1 meter)):
-  switch (state)
-  {
-    case 0:
-      // tell the operatior what to do
-      printf("# started mission 2.\n");
-      system("espeak \"looking for ArUco\" -ven+f4 -s130 -a5 2>/dev/null &"); 
-      bridge->send("oled 5 looking 4 ArUco");
-      state=11;
-      break;
-    case 11:
-      // wait for finished driving first part)
-      if (fabsf(bridge->motor->getVelocity()) < 0.001 and bridge->imu->turnrate() < (2*180/M_PI))
-      { // finished first drive and turnrate is zero'ish
-        state = 12;
-        // wait further 30ms - about one camera frame at 30 FPS
-        usleep(35000);
-        // start aruco analysis 
-        printf("# started new ArUco analysis\n");
-        cam->arUcos->setNewFlagToFalse();
-        cam->doArUcoAnalysis = true;
-      }
-      break;
-    case 12:
-      if (not cam->doArUcoAnalysis)
-      { // aruco processing finished
-        if (cam->arUcos->getMarkerCount(true) > 0)
-        { // found a marker - go to marker (any marker)
-          state = 30;
-          // tell the operator
-          printf("# case=%d found marker\n", state);
-          system("espeak \"found marker.\" -ven+f4 -s130 -a5 2>/dev/null &"); 
-          bridge->send("oled 5 found marker");
-        }
-        else
-        { // turn a bit (more)
-          state = 20;
-        }
-      }
-      break;
-    case 20: 
-      { // turn a bit and then look for a marker again
-        int line = 0;
-        snprintf(lines[line++], MAX_LEN, "vel=0.25, tr=0.15: turn=10,time=10");
-        snprintf(lines[line++], MAX_LEN, "vel=0,event=2:dist=1");
-        sendAndActivateSnippet(lines, line);
-        // make sure event 2 is cleared
-        bridge->event->isEventSet(2);
-        // tell the operator
-        printf("# case=%d sent mission turn a bit\n", state);
-        system("espeak \"turn.\" -ven+f4 -s130 -a5 2>/dev/null &"); 
-        bridge->send("oled 5 code turn a bit");
-        state = 21;
-        break;
-      }
-    case 21: // wait until manoeuvre has finished
-      if (bridge->event->isEventSet(2))
-      {// repeat looking (until all 360 degrees are tested)
-        if (featureCnt < 36)
-          state = 11;
-        else
-          state = 999;
-        featureCnt++;
-      }
-      break;
-    case 30:
-      { // found marker
-        // if stop marker, then exit
-        ArUcoVal * v = cam->arUcos->getID(6);
-        if (v != NULL and v->isNew)
-        { // sign to stop
-          state = 999;
-          break;
-        }
-        // use the first (assumed only one)
-        v = cam->arUcos->getFirstNew();
-        v->lock.lock();
-        // marker position in robot coordinates
-        float xm = v->markerPosition.at<float>(0,0);
-        float ym = v->markerPosition.at<float>(0,1);
-        float hm = v->markerAngle;
-        // stop some distance in front of marker
-        float dx = 0.3; // distance to stop in front of marker
-        float dy = 0.0; // distance to the left of marker
-        xm += - dx*cos(hm) + dy*sin(hm);
-        ym += - dx*sin(hm) - dy*cos(hm);
-        // limits
-        float acc = 1.0; // max allowed acceleration - linear and turn
-        float vel = 0.3; // desired velocity
-        // set parameters
-        // end at 0 m/s velocity
-        UPose2pose pp4(xm, ym, hm, 0.0);
-        printf("\n");
-        // calculate turn-straight-turn (Angle-Line-Angle)  manoeuvre
-        bool isOK = pp4.calculateALA(vel, acc);
-        // use only if distance to destination is more than 3cm
-        if (isOK and (pp4.movementDistance() > 0.03))
-        { // a solution is found - and more that 3cm away.
-          // debug print manoeuvre details
-          pp4.printMan();
-          printf("\n");
-          // debug end
-          int line = 0;
-          if (pp4.initialBreak > 0.01)
-          { // there is a starting straight part
-            snprintf(lines[line++], MAX_LEN, "vel=%.3f,acc=%.1f :dist=%.3f", 
-                     pp4.straightVel, acc, pp4.straightVel);
-          }
-          snprintf(lines[line++], MAX_LEN,   "vel=%.3f,tr=%.3f :turn=%.1f", 
-                   pp4.straightVel, pp4.radius1, pp4.turnArc1 * 180 / M_PI);
-          snprintf(lines[line++], MAX_LEN,   ":dist=%.3f", pp4.straightDist);
-          snprintf(lines[line++], MAX_LEN,   "tr=%.3f :turn=%.1f", 
-                   pp4.radius2, pp4.turnArc2 * 180 / M_PI);
-          if (pp4.finalBreak > 0.01)
-          { // there is a straight break distance
-            snprintf(lines[line++], MAX_LEN,   "vel=0 : time=%.2f", 
-                     sqrt(2*pp4.finalBreak));
-          }
-          snprintf(lines[line++], MAX_LEN,   "vel=0, event=2: dist=1");
-          sendAndActivateSnippet(lines, line);
-          // make sure event 2 is cleared
-          bridge->event->isEventSet(2);
-          //
-          // debug
-          for (int i = 0; i < line; i++)
-          { // print sent lines
-            printf("# line %d: %s\n", i, lines[i]);
-          }
-          // debug end
-          // tell the operator
-          printf("# Sent mission snippet to marker (%d lines)\n", line);
-          //system("espeak \"code snippet to marker.\" -ven+f4 -s130 -a20 2>/dev/null &"); 
-          bridge->send("oled 5 code to marker");
-          // wait for movement to finish
-          state = 31;
-        }
-        else
-        { // no marker or already there
-          printf("# No need to move, just %.2fm, frame %d\n", 
-                 pp4.movementDistance(), v->frameNumber);
-          // look again for marker
-          state = 11;
-        }
-        v->lock.unlock();
-      }
-      break;
-    case 31:
-      // wait for event 2 (send when finished driving)
-      if (bridge->event->isEventSet(2))
-      { // look for next marker
-        state = 11;
-        // no, stop
-        state = 999;
-      }
-      break;
-    case 999:
-    default:
-      printf("mission 1 ended \n");
-      bridge->send("oled 5 \"mission 1 ended.\"");
-      finished = true;
+  case 0:
+    // tell the operatior what to do
+    printf("# press green to start.\n");
+    system("espeak \"press green to start\" -ven+f4 -s130 -a5 2>/dev/null &");
+    bridge->send("oled 5 press green to start");
+    state++;
+    break;
+  case 1:
+    if (bridge->joy->button[BUTTON_GREEN])
+      state = 10;
+    break;
+  case 10: // first PART - wait for IR2 then go fwd and turn
+    snprintf(lines[0], MAX_LEN, "vel=0 : ir2 < 0.3");
+    // drive straight 0.6m - keep an acceleration limit of 1m/s2 (until changed)
+    snprintf(lines[1], MAX_LEN, "vel=0.2,acc=1:dist=0.6");
+    // stop and create an event when arrived at this line
+    snprintf(lines[2], MAX_LEN, "event=1, vel=0");
+    // add a line, so that the robot is occupied until next snippet has arrived
+    snprintf(lines[3], MAX_LEN, ": dist=1");
+    // send the 4 lines to the REGBOT
+    sendAndActivateSnippet(lines, 4);
+    // make sure event 1 is cleared
+    bridge->event->isEventSet(1);
+    // tell the operator
+    printf("# case=%d sent mission snippet 1\n", state);
+    system("espeak \"code snippet 1.\" -ven+f4 -s130 -a5 2>/dev/null &");
+    bridge->send("oled 5 code snippet 1");
+    //
+    // play as we go
+    play.setFile("../The_thing_goes_Bassim.mp3");
+    play.setVolume(5); // % (0..100)
+    play.start();
+    // go to wait for finished
+    state = 11;
+    featureCnt = 0;
+    break;
+  case 11:
+    // wait for event 1 (send when finished driving first part)
+    if (bridge->event->isEventSet(1))
+    { // finished first drive
+      state = 999;
       play.stopPlaying();
-      break;
+    }
+    break;
+  case 999:
+  default:
+    printf("mission 1 ended \n");
+    bridge->send("oled 5 \"mission 1 ended.\"");
+    finished = true;
+    break;
   }
-  // printf("# mission1 return (state=%d, finished=%d, )\n", state, finished);
   return finished;
 }
-
-
 
 /**
  * Run mission
@@ -596,43 +406,21 @@ bool UMission::mission2(int & state)
  *              therefore defined as reference with the '&'.
  *              State will be 0 at first call.
  * \returns true, when finished. */
-bool UMission::mission3(int & state)
+bool UMission::mission2(int &state)
 {
   bool finished = false;
   switch (state)
   {
-    case 999:
-    default:
-      printf("mission 3 ended\n");
-      bridge->send("oled 5 mission 3 ended.");
-      finished = true;
-      break;
+  case 1:
+
+  default:
+    printf("mission 3 ended\n");
+    bridge->send("oled 5 mission 3 ended.");
+    finished = true;
+    break;
   }
   return finished;
 }
-
-
-/**
- * Run mission
- * \param state is kept by caller, but is changed here
- *              therefore defined as reference with the '&'.
- *              State will be 0 at first call.
- * \returns true, when finished. */
-bool UMission::mission4(int & state)
-{
-  bool finished = false;
-  switch (state)
-  {
-    case 999:
-    default:
-      printf("mission 4 ended\n");
-      bridge->send("oled 5 mission 4 ended.");
-      finished = true;
-      break;
-  }
-  return finished;
-}
-
 
 void UMission::openLog()
 {
