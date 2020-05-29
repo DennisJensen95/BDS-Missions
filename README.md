4d3cbe1ffd6af4f22bb6519a509859eff1266494
Dennis <ngh1adj95@hotmail.com>
2020-05-29 06:56:05 +0200

    Image in readme for track and small description

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BDS-Missions
-This is the directory of mission scripts
+This repository holds the source code for completing a mission track with the robot sally.
+
+The file umission.cpp is file containing all the mission sequences.
+
+The file findball.cpp contains the algorithms for finding balls.
+
+The cloud_process.cpp contains the communication protocols for doing cloud processing with neural nets.
+
+Beneath is an image of the track
+
+![img](./images/track.png)
--- modules/FindMosquitto.cmake
+++ modules/FindMosquitto.cmake
@@ -9,13 +9,13 @@ find_library(MOSQUITTO_LIBRARY NAMES libmosquitto mosquitto)
 
 # Handle the QUIETLY and REQUIRED arguments and set MOSQUITTO_FOUND to TRUE if
 # all listed variables are TRUE.
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Mosquitto DEFAULT_MSG MOSQUITTO_LIBRARY MOSQUITTO_INCLUDE_DIR)
+# include(FindPackageHandleStandardArgs)
+# find_package_handle_standard_args(Mosquitto DEFAULT_MSG MOSQUITTO_LIBRARY MOSQUITTO_INCLUDE_DIR)
 
-if(MOSQUITTO_FOUND)
-  set(MOSQUITTO_LIBRARIES ${MOSQUITTO_LIBRARY})
-else(MOSQUITTO_FOUND)
-  set(MOSQUITTO_LIBRARIES)
-endif(MOSQUITTO_FOUND)
+# if(MOSQUITTO_FOUND)
+#   set(MOSQUITTO_LIBRARIES ${MOSQUITTO_LIBRARY})
+# else(MOSQUITTO_FOUND)
+#   set(MOSQUITTO_LIBRARIES)
+# endif(MOSQUITTO_FOUND)
 
-mark_as_advanced(MOSQUITTO_INCLUDE_DIR MOSQUITTO_LIBRARY)
+# mark_as_advanced(MOSQUITTO_INCLUDE_DIR MOSQUITTO_LIBRARY)
